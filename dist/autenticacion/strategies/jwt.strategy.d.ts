238b84edbb027bf03c947efd8652a1e043c3164f
Cristian <cristian.alvarezh07@gmail.com>
2026-07-28 18:33:55 -0500

    cambios

--- dist/autenticacion/strategies/jwt.strategy.d.ts
+++ dist/autenticacion/strategies/jwt.strategy.d.ts
@@ -6,7 +6,7 @@ interface CargaJwt {
     correoElectronico: string;
     rol: string;
 }
-declare const EstrategiaJwt_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
+declare const EstrategiaJwt_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class EstrategiaJwt extends EstrategiaJwt_base {
